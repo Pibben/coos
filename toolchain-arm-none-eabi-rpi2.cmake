2441cbe98a5ea22e443fe63e59dcdeedffbd263e
Per Zetterlund <per@pz.se>
2016-04-10 14:49:04 +0200

    Set CPU to armv7 to enable WFI.

--- toolchain-arm-none-eabi-rpi2.cmake
+++ toolchain-arm-none-eabi-rpi2.cmake
@@ -15,12 +15,23 @@ set( CMAKE_OBJCOPY      ${TC_PATH}${CROSS_COMPILE}objcopy
     CACHE FILEPATH "The toolchain objcopy command " FORCE )
 
 # Set the CMAKE C flags (which should also be used by the assembler!
-#set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=neon-vfpv4" )
-#set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=hard" )
-#set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7-a" )
-#set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mtune=cortex-a7" )
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-vfpv4" )
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfloat-abi=hard" )
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7-a" )
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mtune=cortex-a7" )
+
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=neon-vfpv4" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=hard" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7-a" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mtune=cortex-a7" )
+
+set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -mfpu=neon-vfpv4" )
+set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -mfloat-abi=hard" )
+set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=armv7-a" )
+set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -mtune=cortex-a7" )
 
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
-set( CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "" )
+set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS}" CACHE STRING "" )
 
 add_definitions( -DRPI=2 )
