8f9d27c54749115c4d8e74c7f7b93f15a0696df0
Per Zetterlund <per@pz.se>
2018-12-22 19:58:01 +0100

    Update toolchain

--- toolchain-arm-none-eabi-rpi2.cmake
+++ toolchain-arm-none-eabi-rpi2.cmake
@@ -1,15 +1,15 @@
 
-include(CMakeForceCompiler)
-
 set( CMAKE_SYSTEM_NAME          Generic )
 set( CMAKE_SYSTEM_PROCESSOR     BCM2836 )
 
-set( TC_PATH "" )
+set( CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY )
 
-set( CROSS_COMPILE arm-none-eabi- )
+set( TC_PATH "/opt/gcc-arm-8.2-2018.11-x86_64-arm-eabi/bin/" )
 
-CMAKE_FORCE_C_COMPILER( ${TC_PATH}${CROSS_COMPILE}gcc GNU )
-CMAKE_FORCE_CXX_COMPILER( ${TC_PATH}${CROSS_COMPILE}g++ GNU )
+set( CROSS_COMPILE arm-eabi- )
+
+set( CMAKE_C_COMPILER ${TC_PATH}${CROSS_COMPILE}gcc )
+set( CMAKE_CXX_COMPILER ${TC_PATH}${CROSS_COMPILE}g++ )
 
 set( CMAKE_OBJCOPY      ${TC_PATH}${CROSS_COMPILE}objcopy
     CACHE FILEPATH "The toolchain objcopy command " FORCE )
@@ -30,8 +30,8 @@ set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -mfloat-abi=hard" )
 set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=armv7-a" )
 set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -mtune=cortex-a7" )
 
-set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
-set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} " CACHE STRING "" )
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions -fno-rtti -fno-threadsafe-statics" CACHE STRING "" )
 set( CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS}" CACHE STRING "" )
 
 add_definitions( -DRPI=2 )
